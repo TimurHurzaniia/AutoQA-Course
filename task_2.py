2aea6581bd60eb08191ab14a70faf6e0b1f21520
Timur_Hurzaniia <gurzaniya@gmail.com>
2019-03-02 08:03:35 +0200

    @elpiankova task_2 remastered

--- task_2.py
+++ task_2.py
@@ -1,5 +1,8 @@
 def pre_min(*args):
     res = sorted(args)
-    return res[1]
+    i = 1
+    while res[i] == res[0]:
+        i += 1
+    return res[i]
 
-print(pre_min(3,2,9,4,22,11))
+print(pre_min(3,2,1,1,9,4,22,11))
